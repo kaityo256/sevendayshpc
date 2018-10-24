1b88df3aa26e81b1ac0b17d3fbc04c0286f47967
H. Watanabe <kaityo@users.sourceforge.jp>
2018-10-24 22:17:05 +0900

    ハイブリッド並列の手前まで

---
+++ day6/gs_omp1.cpp
@@ -0,0 +1,95 @@
+#include <cstdio>
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <chrono>
+#include <omp.h>
+
+const int L = 128;
+const int TOTAL_STEP = 20000;
+const double F = 0.04;
+const double k = 0.06075;
+const double dt = 0.2;
+const double Du = 0.05;
+const double Dv = 0.1;
+
+typedef std::vector<double> vd;
+
+void init(vd &u, vd &v) {
+  int d = 3;
+  for (int i = L / 2 - d; i < L / 2 + d; i++) {
+    for (int j = L / 2 - d; j < L / 2 + d; j++) {
+      u[j + i * L] = 0.7;
+    }
+  }
+  d = 6;
+  for (int i = L / 2 - d; i < L / 2 + d; i++) {
+    for (int j = L / 2 - d; j < L / 2 + d; j++) {
+      v[j + i * L] = 0.9;
+    }
+  }
+}
+
+double calcU(double tu, double tv) {
+  return tu * tu * tv - (F + k) * tu;
+}
+
+double calcV(double tu, double tv) {
+  return -tu * tu * tv + F * (1.0 - tv);
+}
+
+double laplacian(int ix, int iy, vd &s) {
+  double ts = 0.0;
+  ts += s[ix - 1 + iy * L];
+  ts += s[ix + 1 + iy * L];
+  ts += s[ix + (iy - 1) * L];
+  ts += s[ix + (iy + 1) * L];
+  ts -= 4.0 * s[ix + iy * L];
+  return ts;
+}
+
+void calc(vd &u, vd &v, vd &u2, vd &v2) {
+  for (int iy = 1; iy < L - 1; iy++) {
+    #pragma omp parallel for
+    for (int ix = 1; ix < L - 1; ix++) {
+      double du = 0;
+      double dv = 0;
+      const int i = ix + iy * L;
+      du = Du * laplacian(ix, iy, u);
+      dv = Dv * laplacian(ix, iy, v);
+      du += calcU(u[i], v[i]);
+      dv += calcV(u[i], v[i]);
+      u2[i] = u[i] + du * dt;
+      v2[i] = v[i] + dv * dt;
+    }
+  }
+}
+
+void save_as_dat(vd &u) {
+  static int index = 0;
+  char filename[256];
+  sprintf(filename, "conf%03d.dat", index);
+  std::cout << filename << std::endl;
+  std::ofstream ofs(filename, std::ios::binary);
+  ofs.write((char *)(u.data()), sizeof(double)*L * L);
+  index++;
+}
+
+int main() {
+  const int V = L * L;
+  vd u(V, 0.0), v(V, 0.0);
+  vd u2(V, 0.0), v2(V, 0.0);
+  init(u, v);
+  const auto s = std::chrono::system_clock::now();
+  for (int i = 0; i < TOTAL_STEP; i++) {
+    if (i & 1) {
+      calc(u2, v2, u, v);
+    } else {
+      calc(u, v, u2, v2);
+    }
+  }
+  const auto e = std::chrono::system_clock::now();
+  const auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(e   - s).count();
+  printf("%d threads %lld [ms]\n", omp_get_max_threads(), elapsed);
+  save_as_dat(u);
+}
