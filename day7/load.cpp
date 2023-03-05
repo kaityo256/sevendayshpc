546bcdac024bd47fd1c0d8abae86fd394786d960
Sho Hirose <sho.hirose@gmail.com>
2023-03-05 21:38:37 +0900

    refactor: use alignas for portability

--- day7/load.cpp
+++ day7/load.cpp
@@ -11,7 +11,7 @@ void print256d(__m256d x) {
   printf("%f %f %f %f\n", y[3], y[2], y[1], y[0]);
 }
 
-__attribute__((aligned(32))) double a[] = {0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0};
+double alignas(32) a[] = {0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0};
 
 int main(void) {
   __m256d v1 = _mm256_load_pd(a);
